04fade405b0ad0b96e076e192dd35f6a80ca44c4
LUISARTURCORREIA <72110275+LUISARTURCORREIA@users.noreply.github.com>
2020-09-29 20:23:21 -0300

    Exercício 03

---
+++ exercicio03/src/exercicio03/Exercicio03.java
@@ -0,0 +1,12 @@
+package exercicio03;
+
+public class Exercicio03 {
+
+    public static void main(String[] args) {
+      double porcentagem;
+      double valor;
+      double resultado; 
+      resultado = (valor * porcentagem) /100;
+    }
+    
+}
